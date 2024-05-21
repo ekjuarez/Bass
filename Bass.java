2e54d944bd68317c29555d1ead1672a03201e8e1
Evan Knudsen <ekjuarez20@gmail.com>
2024-05-20 21:58:22 -0600

    Added Bass.java

---
+++ Bass.java
@@ -0,0 +1,11 @@
+public class Bass {
+    public static void main(String[] args) {
+        System.out.println("Hello stupid bass.");
+    }
+    public static double fahrenheitToCelsius (double fahrenheit) {
+        return (fahrenheit - 32) * (5.0 / 9.0);
+    }
+    public static double celsiusToFahrenheit (double celsius) {
+        return ((9.0 / 5.0) * celsius) + 32;
+    }
+}
